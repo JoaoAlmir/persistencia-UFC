9290792fc355d7894f60624059a821736f3e5ae1
JoaoAlmir <junivion@gmail.com>
2023-09-04 14:59:04 -0300

    fix atv 2

--- atv2/Ex1.java
+++ atv2/Ex1.java
@@ -1,44 +1,25 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-
-public class Ex1{
-    public static void main(String[] args){
-        
+public class Ex1 {
+    public static void main(String[] args) {
         try {
+            long ini = System.currentTimeMillis();
 
-        long ini = System.currentTimeMillis();
-
-
-        InputStream is = new FileInputStream(args[0]);
-        InputStreamReader isr = new InputStreamReader(is);
-        BufferedReader br = new BufferedReader(isr);
-        String s = br.readLine();
-
-        PrintStream ps = new PrintStream(args[1]);
-
-        while (s != null) {
-            ps.println(s);
-            s = br.readLine();
-        }
-        ps.close();
-        br.close();
-
-        long fim = System.currentTimeMillis();
-        System.out.println("Tempo de execução: " + (fim - ini) + " ms");
-        
+            byte[] bytes = Files.readAllBytes(Paths.get(args[0]));
+            FileOutputStream is = new FileOutputStream(args[1]);
             
+            is.write(bytes);
+            
+            long fim = System.currentTimeMillis();
+
+            System.out.println("Tempo de execução: " + (fim - ini) + " ms");
+
+            is.close();
         } catch (IOException e) {
-            System.out.println("Erro na entrada ou saida");
+            System.err.println("Erro na entrada ou saida");
         }
-
-        
-
-
-
     }
 }
