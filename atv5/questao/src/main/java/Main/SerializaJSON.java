19349197d5e7060bfa71ec15154accf0a91e3ab2
JoaoAlmir <junivion@gmail.com>
2023-09-18 21:39:22 -0300

    change id, add SerializaJson

--- atv5/questao/src/main/java/Main/SerializaJSON.java
+++ atv5/questao/src/main/java/Main/SerializaJSON.java
@@ -3,10 +3,10 @@ package Main;
 import java.io.File;
 import java.util.List;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
-public class SerializaXML {
+public class SerializaJSON {
     public static void main(String[] args) throws Exception {
         Jogo j1 = new Jogo(1, "Pac Man", "Ação", "come come", "Harry potter", "Hanna Montana", 100.0, 3.0);
         Jogo j2 = new Jogo(2, "Pou", "Sobrevivencia", "come come", "Silvio Santos", "Google", 0.0, 8.0);
@@ -14,9 +14,9 @@ public class SerializaXML {
 
         ListaJogo lj = new ListaJogo(List.of(j1, j2, j3));
 
-        File f = new File("./src/main/java/Main/jogos.xml");
-        XmlMapper xm = new XmlMapper();
-        xm.enable(SerializationFeature.INDENT_OUTPUT);
-        xm.writeValue(f, lj);
+        File f = new File("./src/main/java/Main/jogos.json");
+        ObjectMapper om = new ObjectMapper();
+        om.enable(SerializationFeature.INDENT_OUTPUT);
+        om.writeValue(f, lj);
     }
 }
--- atv5/questao/src/main/java/Main/SerializaJava.java
+++ atv5/questao/src/main/java/Main/SerializaJava.java
@@ -8,7 +8,7 @@ public class SerializaJava {
     public static void main(String[] args) {
         Jogo j1 = new Jogo(1, "Pac Man", "Ação", "come come", "Harry potter", "Hanna Montana", 100.0, 3.0);
         Jogo j2 = new Jogo(2, "Pou", "Sobrevivencia", "come come", "Silvio Santos", "Google", 0.0, 8.0);
-        Jogo j3 = new Jogo(2, "Rift", "MMO", "farmaa", "Thor", "UFC", 0.0, 8.0);
+        Jogo j3 = new Jogo(3, "Rift", "MMO", "farmaa", "Thor", "UFC", 0.0, 8.0);
 
         ListaJogo lj = new ListaJogo(List.of(j1, j2, j3));
 
